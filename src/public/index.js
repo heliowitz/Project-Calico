1db53a0f8e5eb10a805fe90ed20a25b70628eb60
jack.ding <jackding.email@gmail.com>
2017-01-02 00:11:10 -0500

    Fixed header colors

--- src/public/index.js
+++ src/public/index.js
@@ -10,21 +10,53 @@ $(window).on('load', function(){
 
 			$('body').addClass('overlayHide');
 			$('body').removeClass('overlayShow');
+
+			$('body').removeClass('aboutFocus');
+			$('body').removeClass('aboutHidden');
+			$('body').removeClass('projectsFocus');
+			$('body').removeClass('projectsHidden');
+			$('body').removeClass('resumeFocus');
+			$('body').removeClass('resumeHidden');
+
+			$('body').addClass('aboutDefault');
+			$('body').addClass('projectsDefault');
+			$('body').addClass('resumeDefault');
 		} 
 		else {
 			if ($('body').hasClass('projectsShow')){ // Switch from Projects
-				$('body').addClass('projectsSwitch');
+				$('body').addClass('projectsHide');
 				$('body').removeClass('projectsShow');
+
+				$('body').removeClass('projectsFocus');
+				$('body').removeClass('aboutHidden');
+
+				$('body').addClass('aboutFocus');
+				$('body').addClass('projectsHidden');
+
 			} else if ($('body').hasClass('resumeShow')) { // Switch from Resume
-				$('body').addClass('resumeSwitch');
+				$('body').addClass('resumeHide');
 				$('body').removeClass('resumeShow');
+
+				$('body').removeClass('resumeFocus');
+				$('body').removeClass('aboutHidden');
+
+				$('body').addClass('aboutFocus');
+				$('body').addClass('resumeHidden');
+
 			} else { // Show
 				$('body').addClass('overlayShow');
 				$('body').removeClass('overlayHide');
-			}
-			$('body').addClass('aboutShow');
-			$('body').removeClass('aboutSwitch');	
-			$('body').removeClass('aboutHide');			
+
+				$('body').removeClass('aboutDefault');
+				$('body').removeClass('projectsDefault');
+				$('body').removeClass('resumeDefault');
+
+				$('body').addClass('aboutFocus');
+				$('body').addClass('projectsHidden');
+				$('body').addClass('resumeHidden');			
+				}
+				$('body').addClass('aboutShow');
+				$('body').removeClass('aboutHide');			
 		}	
 	});
 
@@ -36,21 +68,54 @@ $(window).on('load', function(){
 
 			$('body').addClass('overlayHide');
 			$('body').removeClass('overlayShow');
+
+			$('body').removeClass('aboutFocus');
+			$('body').removeClass('aboutHidden');
+			$('body').removeClass('projectsFocus');
+			$('body').removeClass('projectsHidden');
+			$('body').removeClass('resumeFocus');
+			$('body').removeClass('resumeHidden');
+
+			$('body').addClass('aboutDefault');
+			$('body').addClass('projectsDefault');
+			$('body').addClass('resumeDefault');
 		} 
 		else {
 			if ($('body').hasClass('aboutShow')){ // Switch from About
-				$('body').addClass('aboutSwitch');
+				$('body').addClass('aboutHide');
 				$('body').removeClass('aboutShow');
+
+				$('body').removeClass('aboutFocus');
+				$('body').removeClass('projectsHidden');
+
+				$('body').addClass('projectsFocus');
+				$('body').addClass('aboutHidden');
+
 			} else if ($('body').hasClass('resumeShow')) { // Switch from Resume
-				$('body').addClass('resumeSwitch');
+				$('body').addClass('resumeHide');
 				$('body').removeClass('resumeShow');
+
+				$('body').removeClass('resumeFocus');
+				$('body').removeClass('projectsHidden');
+
+				$('body').addClass('projectsFocus');
+				$('body').addClass('resumeHidden');
+
 			} else { // Show
 				$('body').addClass('overlayShow');
 				$('body').removeClass('overlayHide');
+
+				$('body').removeClass('aboutDefault');
+				$('body').removeClass('projectsDefault');
+				$('body').removeClass('resumeDefault');
+
+				$('body').addClass('aboutHidden');
+				$('body').addClass('projectsFocus');
+				$('body').addClass('resumeHidden');	
+
 			}
 			$('body').addClass('projectsShow');
 			$('body').removeClass('projectsHide');	
-			$('body').removeClass('projectsSwitch')		
 		}	
 	});
 
@@ -62,21 +127,54 @@ $(window).on('load', function(){
 
 			$('body').addClass('overlayHide');
 			$('body').removeClass('overlayShow');
+
+			$('body').removeClass('aboutFocus');
+			$('body').removeClass('aboutHidden');
+			$('body').removeClass('projectsFocus');
+			$('body').removeClass('projectsHidden');
+			$('body').removeClass('resumeFocus');
+			$('body').removeClass('resumeHidden');
+
+			$('body').addClass('aboutDefault');
+			$('body').addClass('projectsDefault');
+			$('body').addClass('resumeDefault');
 		} 
 		else {
 			if ($('body').hasClass('projectsShow')){ // Switch from Projects
-				$('body').addClass('projectsSwitch');
+				$('body').addClass('projectsHide');
 				$('body').removeClass('projectsShow');
+
+				$('body').removeClass('projectsFocus');
+				$('body').removeClass('resumeHidden');
+
+				$('body').addClass('resumeFocus');
+				$('body').addClass('projectsHidden');
+
 			} else if ($('body').hasClass('aboutShow')) { // Switch from About
-				$('body').addClass('aboutSwitch');
+				$('body').addClass('aboutHide');
 				$('body').removeClass('aboutShow');
+
+				$('body').removeClass('aboutFocus');
+				$('body').removeClass('resumeHidden');
+
+				$('body').addClass('resumeFocus');
+				$('body').addClass('aboutHidden');
+
 			} else { // Show
 				$('body').addClass('overlayShow');
 				$('body').removeClass('overlayHide');
+
+				$('body').removeClass('aboutDefault');
+				$('body').removeClass('projectsDefault');
+				$('body').removeClass('resumeDefault');
+
+				$('body').addClass('aboutHidden');
+				$('body').addClass('projectsHidden');
+				$('body').addClass('resumeFocus');	
+
 			}
 			$('body').addClass('resumeShow');
 			$('body').removeClass('resumeHide');			
-			$('body').removeClass('resumeSwitch');
 		}	
 	});
 });
