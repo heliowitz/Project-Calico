c02eaf24b457821a99b7d6462ca94b3feb6f9da2
jack.ding <jackding.email@gmail.com>
2017-01-03 18:43:35 -0500

    Converted to EJS, Restructured project

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express')  
+const express = require('express')
 const app = express()  
 const port = 3000
 
+app.set('view engine', 'ejs');
+
 var path = require('path');
-app.use(express.static(path.join(__dirname, '/src/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/public/index.html'));
+    // res.sendFile(path.join(__dirname + '/src/public/index.html'));
+    res.render('pages/index.ejs');
 });
 
 app.listen(port, (err) => {  
