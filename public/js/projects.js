c19c866bdcdcf8c827a44b80f7b3b7f1bfb8644b
jack.ding <jackding.email@gmail.com>
2017-01-03 23:01:54 -0500

    Integrated Masonry

---
+++ public/js/projects.js
@@ -0,0 +1,10 @@
+// var grid = document.querySelector('.grid');
+// var msnry = new Masonry( grid, {
+//   // options...
+//   itemSelector: '.grid-item',
+//   columnWidth: 300
+// });
+
+// $('.carousel').carousel({
+//   interval: 1000 * 15
+// });
--- server.js
+++ server.js
@@ -7,9 +7,113 @@ app.set('view engine', 'ejs');
 var path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
 
+var projectTileData = [
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Fitbit",
+		projType:"Desktop Client",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Schulich Dentistry",
+		projType:"Data Analysis Client",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Graphic Designers",
+		projType:"Website",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Movies DB",
+		projType:"MySQL Database Client",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"TCP Packet Guaranteed",
+		projType:"Client / Server Program",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Pacman AI",
+		projType:"Minimax Agent",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Asteroids",
+		projType:"Java Game",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Outer Space",
+		projType:"Java Simulation",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Personal Site",
+		projType:"Website",
+		tileIcon:""
+	},
+	{
+		tileImg:[
+			"./assets/projectImgs/test/test1.png",
+			"./assets/projectImgs/test/test2.png",
+			"./assets/projectImgs/test/test3.png"
+		],
+		projName:"Personal Site",
+		projType:"Website",
+		tileIcon:""
+	}
+];
+
 app.get('/', function(req, res) {
-    // res.sendFile(path.join(__dirname + '/src/public/index.html'));
-    res.render('pages/index.ejs');
+    res.render('pages/index.ejs', {
+    	data:projectTileData
+    });
 });
 
 app.listen(port, (err) => {  
